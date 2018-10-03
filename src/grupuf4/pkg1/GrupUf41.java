1b7e89c06dceaa79a2436bb4a17d00e198b5862b
X541U <X541U@LAPTOP-8M3QRRGJ>
2018-10-03 16:54:40 +0200

    afegit codi Pol

--- src/grupuf4/pkg1/GrupUf41.java
+++ src/grupuf4/pkg1/GrupUf41.java
@@ -1,7 +1,11 @@
 package grupuf4.pkg1;
-public class GrupUf41 {
-    public static void main(String[] args) {
 
+public class GrupUf41 {
+
+    public class PolMinanaRuiz {
+        private void pol(){}
+        private void mario(){}
+        private void roger(){}
     }
-    
+
 }
